d9041afbf895209a47174c6c60c797df6139f503
aidangaetano11 <aidan.gaetano@wsu.edu>
2022-09-30 00:53:08 -0700

    Fixed 3D Sound Issues

--- SteamMultiplayer/Assets/Scripts/WorldEvents/PlayerAudioHelper.cs
+++ SteamMultiplayer/Assets/Scripts/WorldEvents/PlayerAudioHelper.cs
@@ -9,7 +9,10 @@ public class PlayerAudioHelper : MonoBehaviour
 
     public void PlaySound()
     {
-        gameObject.GetComponentInParent<Interactor>().PlayFootstep();     //call interactor to see if this player has control  
+        footstepSound.clip = footstepClips[Random.Range(0, footstepClips.Count)];
+        footstepSound.Play();
+
+        //gameObject.GetComponentInParent<Interactor>().PlayFootstep();     //call interactor to see if this player has control  
     }
 
     public void AuthorityPlaySound()    //if it does, we get sent back to play the sound.
