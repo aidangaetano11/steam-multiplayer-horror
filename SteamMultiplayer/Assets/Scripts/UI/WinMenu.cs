8d84c0e7b75fe7ecbb9f4f56bafadc8f2c066dda
aidangaetano11 <aidan.gaetano@wsu.edu>
2022-10-13 15:51:20 -0700

    WinScreen Fix 3

--- SteamMultiplayer/Assets/Scripts/UI/WinMenu.cs
+++ SteamMultiplayer/Assets/Scripts/UI/WinMenu.cs
@@ -31,6 +31,7 @@ public class WinMenu : NetworkBehaviour
         Debug.Log("HANDLING MONSTER DEATH");
         if (isServer)                //if we are server
         {
+            Cursor.lockState = CursorLockMode.None;
             RpcShowWinMenu();   //we will check if monster is killed, if it is, then show win screen 
         }
         else CmdHandleMonsterDeath();     //if we arent server, call command function
